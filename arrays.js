17de74f838b5af003323403e352e8a62788a70ca
Khangelani Mhlakane <kmhlakane@gmail.com>
2025-10-31 10:02:47 +0200

    This is The array Containing Colors and Numbers

---
+++ arrays.js
@@ -0,0 +1,8 @@
+
+const colors = ['Yellow' ,'Red','Blue'];
+colors.push('Purple' , 'Green', 'Silver');
+const numbers = [1 , 2 , 3 , 4 , 5];
+colors.pop();
+colors.unshift('Black');
+console.log('The lenght of the colors is :', colors.lenght);
+console.log('the lenght of the numbers is :', numbers.lenght);
